6286116ab45d83f05fabcd35cba6e4452cebf6e6
jpcl10 <cardosojoaopedro369@gmail.com>
2026-07-28 09:08:50 -0300

    hsghs

--- prisma/migrations/20260728120000_add_tablet_mode_enums/migration.sql
+++ prisma/migrations/20260728120000_add_tablet_mode_enums/migration.sql
@@ -1,6 +1,41 @@
-ALTER TYPE "PaymentContextType" ADD VALUE IF NOT EXISTS 'TABLET';
-ALTER TYPE "PrintingSource" ADD VALUE IF NOT EXISTS 'TABLET';
-ALTER TYPE "DeviceType" ADD VALUE IF NOT EXISTS 'TABLET';
-ALTER TYPE "SettingsChannel" ADD VALUE IF NOT EXISTS 'TABLET';
-ALTER TYPE "OrderSource" ADD VALUE IF NOT EXISTS 'TABLET';
-ALTER TYPE "CustomerSource" ADD VALUE IF NOT EXISTS 'TABLET';
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_type WHERE typname = 'PaymentContextType') THEN
+    ALTER TYPE "PaymentContextType" ADD VALUE IF NOT EXISTS 'TABLET';
+  END IF;
+END $$;
+
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_type WHERE typname = 'PrintingSource') THEN
+    ALTER TYPE "PrintingSource" ADD VALUE IF NOT EXISTS 'TABLET';
+  END IF;
+END $$;
+
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_type WHERE typname = 'DeviceType') THEN
+    ALTER TYPE "DeviceType" ADD VALUE IF NOT EXISTS 'TABLET';
+  END IF;
+END $$;
+
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_type WHERE typname = 'SettingsChannel') THEN
+    ALTER TYPE "SettingsChannel" ADD VALUE IF NOT EXISTS 'TABLET';
+  END IF;
+END $$;
+
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_type WHERE typname = 'OrderSource') THEN
+    ALTER TYPE "OrderSource" ADD VALUE IF NOT EXISTS 'TABLET';
+  END IF;
+END $$;
+
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_type WHERE typname = 'CustomerSource') THEN
+    ALTER TYPE "CustomerSource" ADD VALUE IF NOT EXISTS 'TABLET';
+  END IF;
+END $$;
